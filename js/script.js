6da8f076d0b3fbc309022b3628bee2dc7e1a765b
maria-auxiliadora <104119263+maria-auxiliadora@users.noreply.github.com>
2022-04-24 02:38:01 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,55 @@
 $(document).ready(function() {
-    $(".show_modal, .hide_modal").on("click", function() {
-        $("body").toggleClass("page-category");
-        $(".modal_cont").toggleClass("modal_cont_animate");
+    // navbar active
+    navbarActive();
+    function navbarActive() {
+        $(".navbar .nav_link").on("click", function() {
+            $(".nav_link").removeClass("active");
+            $(this).addClass("active");
+        });
+
+        $(".nav__block_ux .nav_ux").on("click", function() {
+            $(".nav_ux").removeClass("active");
+            $(this).addClass("active");
+        });
+    }
+
+    // navbar click
+    navbarClickUl();
+    function navbarClickUl() {
+        $(".inicio").on("click", function() {
+            $('html, body').animate({
+                scrollTop: $(".home").offset().top
+            }, 100);
+        });
+
+        $(".blog").on("click", function() {
+            $('html, body').animate({
+                scrollTop: $(".blogs").offset().top
+            }, 100);
+        });
+    }
+
+    $(".hide_modal").on("click", function() {
+        $("body").removeClass("page-category");
+        $(".modal_cont").removeClass("modal_cont_animate");
     });
+
+    $(".show_modal1").on("click", function() {
+        $("body").addClass("page-category");
+        $(".show_blog1").addClass("modal_cont_animate");
+    });
+
+    $(".show_modal2").on("click", function() {
+        $("body").addClass("page-category");
+        $(".show_blog2").addClass("modal_cont_animate");
+    });
+
+    $(".show_modal3").on("click", function() {
+        $("body").addClass("page-category");
+        $(".show_blog3").addClass("modal_cont_animate");
+    });
+
+    setTimeout(function() {
+        $(".loader_bg").fadeToggle();
+    }, 1760);
 });
