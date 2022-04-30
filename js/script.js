fbbfdf64d627fe15c9133605a56523db45bfe5a4
maria-auxiliadora <104119263+maria-auxiliadora@users.noreply.github.com>
2022-04-30 11:59:35 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,32 +2,78 @@ $(document).ready(function() {
     // navbar active
     navbarActive();
     function navbarActive() {
-        $(".navbar .nav_link").on("click", function() {
-            $(".nav_link").removeClass("active");
-            $(this).addClass("active");
+        var scrollLink = $(".nav_link");
+        var scrollLinkUx = $(".nav_ux");
+
+        // smooth scrolling
+        scrollLink.click(function(e) {
+            e.preventDefault();
+            $("html, body").animate({
+                scrollTop: $(this.hash).offset().top
+            }, 30);
         });
 
-        $(".nav__block_ux .nav_ux").on("click", function() {
-            $(".nav_ux").removeClass("active");
-            $(this).addClass("active");
+        scrollLinkUx.click(function(e) {
+            e.preventDefault();
+            $("html, body").animate({
+                scrollTop: $(this.hash).offset().top
+            }, 30);
+        });
+
+        // active link switching
+        $(window).scroll(function() {
+            var scrollbarLocation = $(this).scrollTop();
+
+            scrollLink.each(function() {
+                var sectionOffset = $(this.hash).offset().top - 30;
+
+                if (sectionOffset <= scrollbarLocation) {
+                    $(".nav_link").removeClass("active");
+                    $(this).addClass("active");
+                }
+            });
+
+            scrollLinkUx.each(function() {
+                var sectionOffset = $(this.hash).offset().top - 30;
+
+                if (sectionOffset <= scrollbarLocation) {
+                    $(".nav_ux").removeClass("active");
+                    $(this).addClass("active");
+                }
+            });
+        });
+
+        // quitar el url #
+        $('a[href*="#"]:not([href="#"])').on("click", function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
+                if (target.length) {
+                    $("html, body").animate({
+                        scrollTop: target.offset().top
+                    }, 30);
+                    return false;
+                }
+            }
         });
     }
 
     // navbar click
-    navbarClickUl();
-    function navbarClickUl() {
-        $(".inicio").on("click", function() {
-            $('html, body').animate({
-                scrollTop: $(".home").offset().top
-            }, 100);
-        });
+    // navbarClickUl();
+    // function navbarClickUl() {
+    //     $(".inicio").on("click", function() {
+    //         $('html, body').animate({
+    //             scrollTop: $(".home").offset().top
+    //         }, 100);
+    //     });
 
-        $(".blog").on("click", function() {
-            $('html, body').animate({
-                scrollTop: $(".blogs").offset().top
-            }, 100);
-        });
-    }
+    //     $(".blog").on("click", function() {
+    //         $('html, body').animate({
+    //             scrollTop: $(".blogs").offset().top
+    //         }, 100);
+    //     });
+    // }
 
     $(".hide_modal").on("click", function() {
         $("body").removeClass("page-category");
