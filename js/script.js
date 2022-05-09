7f976cf24ff55ebc218e0ee169e4271c42457aa2
maria-auxiliadora <104119263+maria-auxiliadora@users.noreply.github.com>
2022-05-08 22:32:25 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -59,43 +59,56 @@ $(document).ready(function() {
         });
     }
 
-    // navbar click
-    // navbarClickUl();
-    // function navbarClickUl() {
-    //     $(".inicio").on("click", function() {
-    //         $('html, body').animate({
-    //             scrollTop: $(".home").offset().top
-    //         }, 100);
-    //     });
-
-    //     $(".blog").on("click", function() {
-    //         $('html, body').animate({
-    //             scrollTop: $(".blogs").offset().top
-    //         }, 100);
-    //     });
-    // }
-
-    $(".hide_modal").on("click", function() {
-        $("body").removeClass("page-category");
-        $(".modal_cont").removeClass("modal_cont_animate");
-    });
-
-    $(".show_modal1").on("click", function() {
-        $("body").addClass("page-category");
-        $(".show_blog1").addClass("modal_cont_animate");
-    });
-
-    $(".show_modal2").on("click", function() {
-        $("body").addClass("page-category");
-        $(".show_blog2").addClass("modal_cont_animate");
-    });
-
-    $(".show_modal3").on("click", function() {
-        $("body").addClass("page-category");
-        $(".show_blog3").addClass("modal_cont_animate");
-    });
-
     setTimeout(function() {
         $(".loader_bg").fadeToggle();
     }, 1760);
 });
+
+// multiple modals
+$.fn.multipleModal = function() {
+    $(this).click(function() {
+        $("body").addClass("page-category");
+        $(".modal_cont").addClass("modal_cont_animate");
+        $(".modal_content_anhost").append('<div class="modal_block_c"><div class="content_hidden-anhost pd-b3r pd024"><div class="description_modal fx fx-fd-c gp18 mr-t32"></div></div></div>');
+
+        var dataTitle = $(this).attr('data-title');
+        var dataDescription = $(this).attr('data-description');
+        $(".content_hidden-anhost").prepend(dataTitle).before();
+        $(".description_modal").prepend(dataDescription);
+
+        $(".hide_modal").on("click", function() {
+            $("body").removeClass("page-category");
+            $(".modal_cont").removeClass("modal_cont_animate");
+
+            setTimeout(function() {
+                $(".modal_block_c").remove();
+            }, 370);
+        });
+    });
+}
+$(".show_modal[data-title]").multipleModal();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
